089fca96a7b6a4544760f5efb6425164d9183995
vamsipri <98684046+vamsipri@users.noreply.github.com>
2025-02-27 18:14:51 +0530

    removed signin

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
-
 import { QueryClient } from '@tanstack/react-query'
-import { createRootRouteWithContext, Outlet, redirect } from '@tanstack/react-router'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { Toaster } from '@/components/ui/toaster'
@@ -10,30 +9,20 @@ import NotFoundError from '@/features/errors/not-found-error'
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient
 }>()({
-  component: RootComponent,
+  component: () => {
+    return (
+      <>
+        <Outlet />
+        <Toaster />
+        {import.meta.env.MODE === 'development' && (
+          <>
+            <ReactQueryDevtools buttonPosition='bottom-left' />
+            <TanStackRouterDevtools position='bottom-right' />
+          </>
+        )}
+      </>
+    )
+  },
   notFoundComponent: NotFoundError,
   errorComponent: GeneralError,
-  beforeLoad: ({ location }) => {
-    // If the user is at the root path, redirect to sign-in
-    if (location.pathname === '/') {
-      throw redirect({
-        to: '/sign-in',
-      })
-    }
-  },
 })
-
-function RootComponent() {
-  return (
-    <>
-      <Outlet />
-      <Toaster />
-      {import.meta.env.MODE === 'development' && (
-        <>
-          <ReactQueryDevtools buttonPosition='bottom-left' />
-          <TanStackRouterDevtools position='bottom-right' />
-        </>
-      )}
-    </>
-  )
-}
